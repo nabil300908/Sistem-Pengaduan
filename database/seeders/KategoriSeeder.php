fc74f0beeadc370995143f5deb2c22e632e5d1c2
nabil300908 <fadlannabil330@gmail.com>
2026-04-12 03:03:52 +0700

    fix kategori seeder

--- database/seeders/KategoriSeeder.php
+++ database/seeders/KategoriSeeder.php
@@ -2,20 +2,22 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\Kategori;
+
 class KategoriSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
-  public function run()
-{
-    $kategoris = ['Fasilitas', 'Kebersihan', 'Keamanan'];
-    foreach ($kategoris as $k) {
-        Kategori::create(['nama_kategori' => $k]);
+    public function run(): void
+    {
+        // Hapus data lama biar nggak dobel
+        Kategori::truncate();
+
+        $kategoris = ['Fasilitas', 'Kebersihan', 'Keamanan'];
+
+        foreach ($kategoris as $k) {
+            Kategori::create([
+                'nama_kategori' => $k
+            ]);
+        }
     }
-    // test perubahan
-}
 }
