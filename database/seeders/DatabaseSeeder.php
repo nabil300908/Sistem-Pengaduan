0f8c3f299129dcf89f4513e35754e5f0de2fcecf
nabil300908 <fadlannabil330@gmail.com>
2026-04-12 04:23:49 +0700

    fix banget banget banget banguuet bangetp

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,10 @@ class DatabaseSeeder extends Seeder
      * Seed the application's database.
      */
     public function run()
-{
-    $this->call([UserSeeder::class]);
-}
+    {
+        $this->call([
+            UserSeeder::class,
+            KategoriSeeder::class,
+        ]);
+    }
 }
